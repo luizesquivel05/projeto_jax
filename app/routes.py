431ed49e9f9ff66d99e8e82f623ddb7f0873430f
luizesquivel05 <luizeduardpontesesquivel@gmail.com>
2024-10-20 16:04:53 -0300

    atualizacao do dia 20.10.2024

--- app/routes.py
+++ app/routes.py
@@ -2,13 +2,18 @@ from flask import Blueprint, render_template, Flask
 
 main = Blueprint("main", __name__)
 
+@main.route("/<path:path>")
+def jax_services(path):
+    try:
+        return render_template(f'{path}.html')
+    except Exception as e:
+        if '.html' in str(e):
+            return render_template('error.html', title="ERROR", error=f"Página ( {str(e)} ) não encontrada")
+        return render_template('error.html', title="ERROR", error=str(e))
+
 @main.route('/')
 def home():
     return render_template('home.html', title='Home')
-
-@main.route("/<path:path>")
-def jax_services(path):
-    return render_template('home.html')
 
 @main.route('/sobre')
 def sobre():
@@ -21,3 +26,11 @@ def galeria():
 @main.route('/contato')
 def contato():
     return render_template('contato.html', title='Contato')
+
+@main.route('/historia')
+def historia():
+    return render_template('historia.html', title='História')
+
+@main.route('/funcionalidades')
+def funcionalidades():
+    return render_template('funcionalidades.html', title='Funcionalidades')
